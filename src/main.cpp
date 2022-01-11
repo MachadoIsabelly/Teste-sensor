aa1b028a8487ac394b8dc84e8a261375f17eef89
Isabelly Oliveira Machado <isabelly.machado1828@gmail.com>
2022-01-11 16:41:00 -0300

    Dual sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
 #include <Arduino.h>
 
+#define sensorA 12
+#define sensorB 25
+#define ledA 5
+#define ledB 19
+
 // alguma coisa
 
 void setup() {
-  pinMode(12, INPUT);
-  pinMode(5, OUTPUT);
+  Serial.begin(115200);
+  pinMode(sensorA, INPUT);
+  pinMode(sensorB, INPUT);
+  pinMode(ledA, OUTPUT);
+  pinMode(ledB, OUTPUT);
+  Serial.println("All pins set");
 }
 
 void loop() {
-  if(digitalRead(12) == LOW){
-    digitalWrite(5, HIGH);
+  if(digitalRead(sensorA) == LOW){
+    digitalWrite(ledA, HIGH);
+    Serial.print("Sensor A detected!");
   } else {
-    digitalWrite(5, LOW);
+    digitalWrite(ledA, LOW);
+    Serial.print("Sensor A away!");
   }
+
+  if(digitalRead(sensorB) == LOW){
+    digitalWrite(ledB, HIGH);
+    Serial.println("    Sensor B detected!");
+  } else {
+    digitalWrite(ledB, LOW);
+    Serial.println("    Sensor B away!");
+  }
+
   delay(100);
 }
