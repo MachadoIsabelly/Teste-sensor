b492cda0b7a1f9739dfa8dfc1a3088a0cab3cddd
Isabelly Oliveira Machado <isabelly.machado1828@gmail.com>
2022-01-11 15:58:38 -0300

    sensor micro-ondas

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,15 @@
 // alguma coisa
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(12, INPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(12) == LOW){
+    digitalWrite(5, HIGH);
+  } else {
+    digitalWrite(5, LOW);
+  }
+  delay(100);
 }
